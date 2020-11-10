40225ca66190bdcfddd021dfd4f9d5330136710e
Jon_Snow_PT <37311649+jonsnowpt@users.noreply.github.com>
2020-11-10 21:08:49 +0000

    Dev Init

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div id="mainDiv" className="App">
+      <div id="drawersTable">
+        <div id="titleDiv">
+          <h1 id="mainTtile">
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h1>
+        </div>
+        <div id="paragDiv">
+          <p>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I’ve got some simple tips to help
+            you make any room feel complete.
+          </p>
+        </div>
+        <div>teste</div>
+      </div>
     </div>
   );
 }
