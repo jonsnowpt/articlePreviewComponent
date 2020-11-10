0ca0733eb139879f3fad216524ae64e7a8b1ca04
Jon_Snow_PT <37311649+jonsnowpt@users.noreply.github.com>
2020-11-10 21:52:02 +0000

    CSS Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import * as React from "react";
 import "./styles.css";
+import UserAvatar from "react-user-avatar";
 
 export default function App() {
   return (
     <div id="mainDiv" className="App">
-      <div id="drawersTable">
-        <div id="titleDiv">
-          <h1 id="mainTtile">
+      <div className="drawersTable">
+        <div className="titleDiv">
+          <h1 className="mainTtile">
             Shift the overall look and feel by adding these wonderful touches to
             furniture in your home
           </h1>
         </div>
-        <div id="paragDiv">
+        <div className="paragDiv">
           <p>
             Ever been in a room and felt like something was missing? Perhaps it
             felt slightly bare and uninviting. I’ve got some simple tips to help
             you make any room feel complete.
           </p>
         </div>
-        <div>teste</div>
+        <div className="grid">
+          <div className="UserAvatar">
+            <UserAvatar
+              size="48"
+              name="Will Binns-Smith"
+              src="./avatar-michelle.jpg"
+            />
+          </div>
+          <div className="userName">Michelle Appleton</div>
+          <div className="col300">300 pixels</div>
+        </div>
+        <div className="clear"></div>
       </div>
     </div>
   );
